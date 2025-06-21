649456c893cb15b41fcad44c02b75c23e655f5fc
gundlashivani <93907868+gundlashivani@users.noreply.github.com>
2025-06-21 21:40:29 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#f5f5dc',
+        peach: '#ffe5b4'
+      }
+    }
+  },
+  plugins: [],
+};
